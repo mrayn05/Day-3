6066edd56e2c3d36274f0f313880df771ba54e10
Max Ryan <maxryan12345@hotmail.com>
2017-05-04 01:32:58 +0100

    done some

---
+++ Rectangle.groovy
@@ -0,0 +1,33 @@
+class Rectangle {
+	Point upLeft;
+	Point downRight;	
+}
+
+class Point {
+    double x
+    double y
+}
+
+//First Point
+Point a = new Point();
+print "enter first x co-ordinate";
+a.x = Double.parseDouble(System.console().readLine());
+print "enter first y co-ordinate";
+a.y = Double.parseDouble(System.console().readLine());
+
+//Second Point
+Point b = new Point;
+print "enter second x co-ordinate";
+b.x = Double.parseDouble(System.console().readLine());
+print "enter second y co-ordinate";
+b.y = Double.parseDouble(System.console().readLine());
+
+//Create Rectangle
+Rectangle rectangle = new Rectangle();
+rectangle.upLeft = a;
+Rectangle. upRight = b;
+
+
+
+
+
