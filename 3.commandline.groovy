6f61b48e354cdcacfea2dbccbfc5eb180b2ecc87
Max Ryan <maxryan12345@hotmail.com>
2017-06-23 21:29:57 +0100

    done

--- 3.commandline.groovy
+++ 3.commandline.groovy
@@ -1,13 +1,53 @@
+//Takes input from user
 System.out.print("Enter an equation: ");
 String str = System.console().readLine();
-String first = str.charAt(0);
-String second = str.charAt(2);
+String first = "";
+String second = "";
+
+//Stores the first number
+for (i = 0; i < str.length(); i++) {
+	if (str.charAt(i) == "+"){
+		break;
+	} else if (str.charAt(i) == "-") {
+		break;
+	} else if (str.charAt(i) == "*") {
+		break;
+	} else if (str.charAt(i) == "/") {
+		break;
+	} else {
+		first += str.charAt(i);
+	}
+	
+}
+
+//Stores the second number(in reverse order)
+for (i = str.length(); i-- > 0;) {
+	if (str.charAt(i) == "+"){
+		break;
+	} else if (str.charAt(i) == "-") {
+		break;
+	} else if (str.charAt(i) == "*") {
+		break;
+	} else if (str.charAt(i) == "/") {
+		break;
+	} else {
+		second += str.charAt(i);
+	}
+}
+
+//Puts second number in correct order
+String newsecond = "";
+for (i = second.length(); i-- > 0;) {
+	newsecond += second.charAt(i);
+}
+
+//Determines what mathematical operation to perform
 if (str.contains("+")) {
-	print (Integer.parseInt(first) + Integer.parseInt(second));
+	print (Integer.parseInt(first) + Integer.parseInt(newsecond));
 } else if (str.contains("-")) {
-	print (Integer.parseInt(first) - Integer.parseInt(second));
+	print (Integer.parseInt(first) - Integer.parseInt(newsecond));
 } else if (str.contains("*")) {
-	print (Integer.parseInt(first) * Integer.parseInt(second));
+	print (Integer.parseInt(first) * Integer.parseInt(newsecond));
 } else if (str.contains("/")) {
-	print (Double.parseDouble(first) / Double.parseDouble(second));
+	print (Double.parseDouble(first) / Double.parseDouble(newsecond));
 }
